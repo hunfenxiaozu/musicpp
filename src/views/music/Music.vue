4a73fb79801d1844c3adc6beb7e8e926830c0fb6
xiaokai <lkrexe@qq.com>
2019-05-08 23:49:40 +0800

    做了二级导航栏，但会有一个莫名其妙的bug

--- src/views/music/Music.vue
+++ src/views/music/Music.vue
@@ -1,14 +1,41 @@
 <template>
+	<el-menu
+		:default-active="activeIndex2"
+		class="el-menu-demo"
+		mode="horizontal"
+		@select="handleSelect"
+		background-color="#409EFF"
+		text-color="#fff"
+		active-text-color="#fff">
+		<el-menu-item index="1">推荐</el-menu-item>
+		<el-menu-item index="2">排行</el-menu-item>
+		<el-menu-item index="3">歌单</el-menu-item>
+		<el-menu-item index="4">歌手</el-menu-item>
+	</el-menu>
+
  
-<div>aaaa</div>
 </template>
 
 <script>
 export default {
+	 data() {
+      return {
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  
+.el-menu--horizontal{
+    display: flex;
+    justify-content: center;
+
+  }
 </style>
