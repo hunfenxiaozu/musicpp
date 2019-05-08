0642017410b08aa191d52f382e9996fb21469b37
xiaokai <lkrexe@qq.com>
2019-05-08 17:07:42 +0800

    修改了iview留下的错误

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import App from '@/App'
 import router from './router'
 import elementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import iView from 'iview'
+
 import normalize from './assets/normalize.css'
 
 
 Vue.config.productionTip = false
 Vue.use(elementUi);
 Vue.use(normalize);
-Vue.use(iView);
+
 
 /* eslint-disable no-new */
 new Vue({
--- src/views/index/components/navbar.vue
+++ src/views/index/components/navbar.vue
@@ -1,29 +1,37 @@
 <template>
   <div class="nav navbar">
-    <div class="navmain">
-      <div class="logo" @click="backhome">logo</div>
-      <router-link to="/music">听</router-link>
-      <router-link to="/bbs">shuo</router-link>
-    </div>
-    <div class="navright">
-      <router-link to="/login">登录</router-link>
-      <router-link to="/logon">注册</router-link>
-      
-    </div>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1"><router-link to="/">logo</router-link></el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">找想听的音乐</template>
+    </el-submenu>
+  </el-submenu>
+  <el-submenu index="3">
+    <template slot="title">说音乐相关的话题</template>
+    </el-submenu>
+  <el-menu-item index="4"><router-link to="/login">登录</router-link></el-menu-item>
+  <el-menu-item index="5"><router-link to="/logon">注册</router-link></el-menu-item>
+</el-menu>
+
+
+    
   </div>
 
 </template>
 
 <script>
 export default {
-  name: 'navbar',
-  data(){
-    return {}
+  data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
     },
     methods: {
-      backhome(){
-        this.router.push("/");
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
       }
+    
 
     }
 
@@ -33,12 +41,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .nav{
-    width: 100%;
+    /*width: 100%;
     height: 50px;
-    background: #eee;
+    background: #eee;*/
     display: flex;
     justify-content: center;
-    line-height: 50px;
+    /*line-height: 50px;*/
   }
   .nav .navmain,.nav .navright{
     display: flex;
