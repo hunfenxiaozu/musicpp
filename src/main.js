3c7cbceb2fedc384656aca4e48eb6e9efa2160f0
ZhouRunCheng <1721730305@qq.com>
2019-05-09 13:15:23 +0800

    上传了一个JSONSERVER文件夹，内容是json服务

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import App from '@/App'
-
 import router from './router'
 import elementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import iView from 'iview'
 import normalize from './assets/normalize.css'
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(elementUi);
 Vue.use(normalize);
-Vue.use(iView);
 
 /* eslint-disable no-new */
 new Vue({
