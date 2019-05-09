327f82f6777c1a7ae76592744ea324bc750d334c
xiaokai <lkrexe@qq.com>
2019-05-09 19:31:23 +0800

    组件化一下小navbar，解决了奇怪的bug

--- src/main.js
+++ src/main.js
@@ -3,20 +3,14 @@ import App from '@/App'
 import router from './router'
 import elementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-<<<<<<< HEAD
-=======
 
->>>>>>> 4a73fb79801d1844c3adc6beb7e8e926830c0fb6
 import normalize from './assets/normalize.css'
 
 
 Vue.config.productionTip = false;
 Vue.use(elementUi);
 Vue.use(normalize);
-<<<<<<< HEAD
-=======
 
->>>>>>> 4a73fb79801d1844c3adc6beb7e8e926830c0fb6
 
 /* eslint-disable no-new */
 new Vue({
--- src/views/bbs/Bbs.vue
+++ src/views/bbs/Bbs.vue
@@ -1,51 +1,20 @@
 <template>
- <el-menu
-		:default-active="activeIndex2"
-		class="el-menu-demo"
-		mode="horizontal"
-		@select="handleSelect"
-		background-color="#409EFF"
-		text-color="#fff"
-		active-text-color="#fff">
-		<el-menu-item index="1">乐器评测</el-menu-item>
-		<el-menu-item index="2">乐理讲解</el-menu-item>
-		<el-submenu index="3">
-		    <template slot="title">乐谱分享</template>
-		    <el-menu-item index="2-1">五线谱</el-menu-item>
-		    <el-menu-item index="2-2">简谱</el-menu-item>
-		    <el-menu-item index="2-3">吉他谱</el-menu-item>
-		    <el-submenu index="2-4">
-		      <template slot="title">其他</template>
-		      <el-menu-item index="2-4-1">口琴</el-menu-item>
-		      <el-menu-item index="2-4-2">选项2</el-menu-item>
-		      <el-menu-item index="2-4-3">选项3</el-menu-item>
-		    </el-submenu>
-		 </el-submenu>
-		<el-menu-item index="4">畅所欲言</el-menu-item>
-	</el-menu>
+ <div id="music">
+		<smallnav />
+		<router-view></router-view>
+	</div>
 
 </template>
 
 <script>
+import smallnav from './components/smallnav.vue'
 export default {
-	data() {
-      return {
-        activeIndex2: '1'
-      };
-    },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
-    }
+	name: 'music',
+    components:{ smallnav }
  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .el-menu--horizontal{
-    display: flex;
-    justify-content: center;
-
-  }
+  
 </style>
